201c24e0908caf7896fae78fe5808b1638b17a3d
lingyangxue <lingyangxue@gmail.com>
2026-09-18 00:39:40 +0800

    Create HAARootListController.m

---
+++ HideAllApps/Preferences/HAARootListController.m
@@ -0,0 +1,69 @@
+#import "HAARootListController.h"
+#import "HAAAppPickerController.h"
+#import <notify.h>
+
+#define kSuiteName @"com.yourname.hideallapps"
+#define kDarwinNotification "com.yourname.hideallapps/prefsChanged"
+
+@implementation HAARootListController
+
+- (NSUserDefaults *)defaults { return [[NSUserDefaults alloc] initWithSuiteName:kSuiteName]; }
+
+- (void)viewDidLoad { [super viewDidLoad]; self.title = @"HideAllApps"; }
+
+- (NSArray *)specifiers {
+    if (!_specifiers) {
+        NSMutableArray *specs = [NSMutableArray array];
+        [specs addObject:[PSSpecifier groupSpecifierWithName:@"功能开关"]];
+
+        PSSpecifier *enable = [PSSpecifier preferenceSpecifierNamed:@"启用插件" target:self set:@selector(setPreferenceValue:specifier:) get:@selector(readPreferenceValue:) detail:nil cell:PSSwitchCell edit:nil];
+        [enable setProperty:@"enabled" forKey:@"key"];
+        [enable setProperty:@NO forKey:@"default"];
+        [specs addObject:enable];
+
+        PSSpecifier *hideAll = [PSSpecifier preferenceSpecifierNamed:@"立即隐藏所有 App" target:self set:@selector(setPreferenceValue:specifier:) get:@selector(readPreferenceValue:) detail:nil cell:PSSwitchCell edit:nil];
+        [hideAll setProperty:@"hideAll" forKey:@"key"];
+        [hideAll setProperty:@NO forKey:@"default"];
+        [specs addObject:hideAll];
+
+        PSSpecifier *group2 = [PSSpecifier groupSpecifierWithName:@"触发手势"];
+        [group2 setProperty:@"选择一个手势来一键隐藏/显示所有桌面 App" forKey:@"footerText"];
+        [specs addObject:group2];
+
+        NSArray *names  = @[@"关闭", @"上滑", @"左滑", @"右滑", @"状态栏单击", @"状态栏双击"];
+        NSArray *values = @[@0, @1, @2, @3, @4, @5];
+        PSSpecifier *gesture = [PSSpecifier preferenceSpecifierNamed:@"触发手势" target:self set:@selector(setPreferenceValue:specifier:) get:@selector(readPreferenceValue:) detail:nil cell:PSLinkListCell edit:nil];
+        [gesture setProperty:@"gestureType" forKey:@"key"];
+        [gesture setProperty:names forKey:@"validTitles"];
+        [gesture setProperty:values forKey:@"validValues"];
+        [gesture setProperty:@0 forKey:@"default"];
+        [specs addObject:gesture];
+
+        PSSpecifier *group3 = [PSSpecifier groupSpecifierWithName:@"单独选择要隐藏的 App"];
+        [group3 setProperty:@"这些 App 会一直隐藏（即使未启用「隐藏所有」）" forKey:@"footerText"];
+        [specs addObject:group3];
+
+        PSSpecifier *pick = [PSSpecifier preferenceSpecifierNamed:@"选择隐藏的 App" target:self set:nil get:nil detail:[HAAAppPickerController class] cell:PSLinkCell edit:nil];
+        [specs addObject:pick];
+
+        _specifiers = specs;
+    }
+    return _specifiers;
+}
+
+- (id)readPreferenceValue:(PSSpecifier *)specifier {
+    NSString *key = [specifier propertyForKey:@"key"];
+    id value = [[self defaults] objectForKey:key];
+    if (value == nil) value = [specifier propertyForKey:@"default"];
+    return value ?: @0;
+}
+
+- (void)setPreferenceValue:(id)value specifier:(PSSpecifier *)specifier {
+    NSString *key = [specifier propertyForKey:@"key"];
+    NSUserDefaults *d = [self defaults];
+    [d setObject:value forKey:key];
+    [d synchronize];
+    notify_post(kDarwinNotification);
+}
+
+@end
