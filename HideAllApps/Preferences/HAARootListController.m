c3078f2e37225b30c534cebc30c53b31eac239f9
lingyangxue <lingyangxue@gmail.com>
2026-09-18 07:48:02 +0800

    Update HAARootListController.m

--- HideAllApps/Preferences/HAARootListController.m
+++ HideAllApps/Preferences/HAARootListController.m
@@ -28,21 +28,32 @@
         [hideAll setProperty:@NO forKey:@"default"];
         [specs addObject:hideAll];
 
-        // ===== 触发手势（开关形式，只能开一个）=====
-        PSSpecifier *group2 = [PSSpecifier groupSpecifierWithName:@"触发手势（打开任意一个即可，会互斥）"];
+        // ===== 隐藏手势（互斥单选）=====
+        PSSpecifier *group2 = [PSSpecifier groupSpecifierWithName:@"隐藏手势（只能选一个）"];
         [group2 setProperty:@"打开其中一个开关后，另一个会自动关闭" forKey:@"footerText"];
         [specs addObject:group2];
-NSArray *names = @[@"关闭", @"上滑", @"左滑", @"右滑", @"状态栏单击", @"状态栏双击", @"摇一摇"];
+
+        NSArray *names = @[@"关闭", @"上滑", @"左滑", @"右滑", @"状态栏单击", @"状态栏双击"];
         for (NSInteger i = 0; i < names.count; i++) {
             PSSpecifier *sp = [PSSpecifier preferenceSpecifierNamed:names[i] target:self set:@selector(setGestureValue:specifier:) get:@selector(gestureValue:) detail:nil cell:PSSwitchCell edit:nil];
             [sp setProperty:@(i) forKey:@"gestureIndex"];
             [specs addObject:sp];
         }
 
-        // ===== 单独隐藏的 App =====
-        PSSpecifier *group3 = [PSSpecifier groupSpecifierWithName:@"单独选择要隐藏的 App"];
-        [group3 setProperty:@"这些 App 会一直隐藏（即使未启用「隐藏所有」）" forKey:@"footerText"];
+        // ===== 摇一摇（独立开关，只隐藏）=====
+        PSSpecifier *group3 = [PSSpecifier groupSpecifierWithName:@"摇一摇（只隐藏，独立开关）"];
+        [group3 setProperty:@"摇一摇手机只隐藏，恢复请用上面的状态栏双击手势" forKey:@"footerText"];
         [specs addObject:group3];
+
+        PSSpecifier *shake = [PSSpecifier preferenceSpecifierNamed:@"启用摇一摇隐藏" target:self set:@selector(setPreferenceValue:specifier:) get:@selector(readPreferenceValue:) detail:nil cell:PSSwitchCell edit:nil];
+        [shake setProperty:@"shakeEnabled" forKey:@"key"];
+        [shake setProperty:@NO forKey:@"default"];
+        [specs addObject:shake];
+
+        // ===== 单独隐藏的 App =====
+        PSSpecifier *group4 = [PSSpecifier groupSpecifierWithName:@"单独选择要隐藏的 App"];
+        [group4 setProperty:@"这些 App 会一直隐藏（即使未启用「隐藏所有」）" forKey:@"footerText"];
+        [specs addObject:group4];
 
         PSSpecifier *pick = [PSSpecifier preferenceSpecifierNamed:@"选择隐藏的 App" target:self set:nil get:nil detail:[HAAAppPickerController class] cell:PSLinkCell edit:nil];
         [specs addObject:pick];
@@ -52,7 +63,6 @@ NSArray *names = @[@"关闭", @"上滑", @"左滑", @"右滑", @"状态栏单击
     return _specifiers;
 }
 
-// ===== 通用 read/set =====
 - (id)readPreferenceValue:(PSSpecifier *)specifier {
     NSString *key = [specifier propertyForKey:@"key"];
     id value = [[self defaults] objectForKey:key];
@@ -68,7 +78,6 @@ NSArray *names = @[@"关闭", @"上滑", @"左滑", @"右滑", @"状态栏单击
     notify_post(kDarwinNotification);
 }
 
-// ===== 手势开关逻辑 =====
 - (id)gestureValue:(PSSpecifier *)specifier {
     NSInteger idx = [[specifier propertyForKey:@"gestureIndex"] integerValue];
     NSInteger cur = [[self defaults] integerForKey:@"gestureType"];
@@ -77,14 +86,12 @@ NSArray *names = @[@"关闭", @"上滑", @"左滑", @"右滑", @"状态栏单击
 
 - (void)setGestureValue:(id)value specifier:(PSSpecifier *)specifier {
     NSInteger idx = [[specifier propertyForKey:@"gestureIndex"] integerValue];
+    NSUserDefaults *d = [self defaults];
     if ([value boolValue]) {
-        NSUserDefaults *d = [self defaults];
         [d setInteger:idx forKey:@"gestureType"];
         [d synchronize];
         notify_post(kDarwinNotification);
     } else {
-        // 用户手动关掉当前选中的，就设为「关闭」(0)
-        NSUserDefaults *d = [self defaults];
         if ([[d objectForKey:@"gestureType"] integerValue] == idx) {
             [d setInteger:0 forKey:@"gestureType"];
             [d synchronize];
