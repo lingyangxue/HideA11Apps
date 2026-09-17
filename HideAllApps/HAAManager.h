68ca10f9df48b798c5a8ba73e7f57e1b446f03a2
lingyangxue <lingyangxue@gmail.com>
2026-09-18 00:34:55 +0800

    Add HAAManager header file with gesture types

---
+++ HideAllApps/HAAManager.h
@@ -0,0 +1,26 @@
+#import <Foundation/Foundation.h>
+
+typedef NS_ENUM(NSInteger, HAAGestureType) {
+    HAAGestureTypeNone = 0,
+    HAAGestureTypeSwipeUp,
+    HAAGestureTypeSwipeLeft,
+    HAAGestureTypeSwipeRight,
+    HAAGestureTypeStatusBarSingleTap,
+    HAAGestureTypeStatusBarDoubleTap,
+};
+
+extern NSString * const kHAASuiteName;
+extern NSString * const kHAAPrefsChangedDarwinNotification;
+
+@interface HAAManager : NSObject
+@property (nonatomic, assign) BOOL enabled;
+@property (nonatomic, assign) NSInteger gestureType;
+@property (nonatomic, assign) BOOL hideAll;
+@property (nonatomic, strong) NSSet *hiddenBundleIDs;
++ (instancetype)sharedManager;
+- (void)reload;
+- (BOOL)shouldHideBundleID:(NSString *)bundleID;
+- (void)toggleHidden;
+- (void)refreshAllIconViews;
+- (void)applyHiddenStateToIconView:(id)iconView;
+@end
