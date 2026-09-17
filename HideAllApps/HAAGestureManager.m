31f0288d468e4985ca568be79157d88ac071a4e2
lingyangxue <lingyangxue@gmail.com>
2026-09-18 07:56:17 +0800

    Update HAAGestureManager.m

--- HideAllApps/HAAGestureManager.m
+++ HideAllApps/HAAGestureManager.m
@@ -76,16 +76,15 @@ static const void *kHAAStatusBarInstalledKey = &kHAAStatusBarInstalledKey;
     if ([self gestureMatches:HAAGestureTypeStatusBarSingleTap]) [self fireToggle];
 }
 
-// 双击状态栏 = 只恢复
 - (void)handleStatusBarDoubleTap:(UITapGestureRecognizer *)gr {
     if (![self gestureMatches:HAAGestureTypeStatusBarDoubleTap]) return;
     [[HAAManager sharedManager] showAllNow];
 }
 
-// 摇一摇 = 只隐藏
 - (void)handleShake {
-    if (![self gestureMatches:HAAGestureTypeShake]) return;
-    [[HAAManager sharedManager] hideAllNow];
+    HAAManager *m = [HAAManager sharedManager];
+    if (!m.enabled || !m.shakeEnabled) return;
+    [m hideAllNow];
 }
 
 - (void)fireToggle {
