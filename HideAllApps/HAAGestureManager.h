bf491ce5876531821f8e3ad1c68adefcad205f7d
lingyangxue <lingyangxue@gmail.com>
2026-09-18 00:36:27 +0800

    Create HAAGestureManager.h

---
+++ HideAllApps/HAAGestureManager.h
@@ -0,0 +1,8 @@
+#import <UIKit/UIKit.h>
+
+@interface HAAGestureManager : NSObject
++ (instancetype)sharedManager;
+- (void)setupGesturesOnView:(UIView *)view;
+- (void)setupStatusBarGestures:(UIView *)view;
+- (void)installGesturesIntoSpringBoard;
+@end
