b1e122fa311dcbb2797e2c183ad3330d338314cd
lingyangxue <lingyangxue@gmail.com>
2026-09-18 23:52:05 +0800

    Update HAAManager.m

--- HideAllApps/HAAManager.m
+++ HideAllApps/HAAManager.m
@@ -40,9 +40,19 @@ NSString * const kHAAPrefsChangedDarwinNotification = @"com.yourname.hideallapps
 
 - (void)reload {
     NSUserDefaults *d = [self defaults];
-    self.enabled    = [d boolForKey:@"enabled"];
-    self.hideAll    = [d boolForKey:@"hideAll"];
-    NSArray *arr    = [d arrayForKey:@"hiddenBundleIDs"] ?: @[];
+    self.enabled         = [d boolForKey:@"enabled"];
+    self.hideAll         = [d boolForKey:@"hideAll"];
+    self.leftDownEnabled = [d boolForKey:@"leftDownEnabled"];
+    self.rightDownEnabled= [d boolForKey:@"rightDownEnabled"];
+
+    id topV = [d objectForKey:@"zoneTopRatio"];
+    self.zoneTopRatio = topV ? [topV doubleValue] : 0.15;
+    id botV = [d objectForKey:@"zoneBottomRatio"];
+    self.zoneBottomRatio = botV ? [botV doubleValue] : 0.85;
+    id widV = [d objectForKey:@"zoneWidth"];
+    self.zoneWidth = widV ? [widV doubleValue] : 150.0;
+
+    NSArray *arr = [d arrayForKey:@"hiddenBundleIDs"] ?: @[];
     self.hiddenBundleIDs = [NSSet setWithArray:arr];
 
     if (self.hideAll && self.enabled) [self startRefreshTimer];
@@ -114,10 +124,7 @@ NSString * const kHAAPrefsChangedDarwinNotification = @"com.yourname.hideallapps
         return;
     }
 
-    if (!self.enabled) {
-        view.alpha = 1.0;
-        return;
-    }
+    if (!self.enabled) { view.alpha = 1.0; return; }
 
     BOOL hide = NO;
     id icon = nil;
@@ -139,7 +146,6 @@ NSString * const kHAAPrefsChangedDarwinNotification = @"com.yourname.hideallapps
         else if (bundleID) hide = [self shouldHideBundleID:bundleID];
     }
 
-    // 只改 alpha
     view.alpha = hide ? 0.0 : 1.0;
 }
 
@@ -157,6 +163,58 @@ NSString * const kHAAPrefsChangedDarwinNotification = @"com.yourname.hideallapps
         return;
     }
     for (UIView *sub in view.subviews) [self _walkView:sub depth:depth + 1];
+}
+
+#pragma mark - 调试边框
+
+- (void)showDebugBorder {
+    if (!self.enabled) return;
+    UIView *host = nil;
+    for (UIWindow *w in [UIApplication sharedApplication].windows) {
+        NSString *cls = NSStringFromClass(w.class);
+        if ([cls containsString:@"StatusBar"]) continue;
+        if ([cls containsString:@"Keyboard"]) continue;
+        if (w.bounds.size.width > 300 && w.bounds.size.height > 600) { host = w; break; }
+    }
+    if (!host) host = [UIApplication sharedApplication].keyWindow;
+    if (!host) return;
+
+    CGSize size = host.bounds.size;
+    for (UIView *v in host.subviews) {
+        if (v.tag == 99991) [v removeFromSuperview];
+    }
+
+    CGFloat y = size.height * self.zoneTopRatio;
+    CGFloat h = size.height * (self.zoneBottomRatio - self.zoneTopRatio);
+
+    UIView *leftBorder = [[UIView alloc] init];
+    leftBorder.tag = 99991;
+    leftBorder.backgroundColor = [UIColor colorWithRed:1.0 green:0.0 blue:0.0 alpha:0.25];
+    leftBorder.layer.borderColor = [UIColor redColor].CGColor;
+    leftBorder.layer.borderWidth = 3.0;
+    leftBorder.userInteractionEnabled = NO;
+    leftBorder.frame = CGRectMake(0, y, self.zoneWidth, h);
+    [host addSubview:leftBorder];
+
+    UIView *rightBorder = [[UIView alloc] init];
+    rightBorder.tag = 99991;
+    rightBorder.backgroundColor = [UIColor colorWithRed:0.0 green:0.0 blue:1.0 alpha:0.25];
+    rightBorder.layer.borderColor = [UIColor blueColor].CGColor;
+    rightBorder.layer.borderWidth = 3.0;
+    rightBorder.userInteractionEnabled = NO;
+    rightBorder.frame = CGRectMake(size.width - self.zoneWidth, y, self.zoneWidth, h);
+    [host addSubview:rightBorder];
+
+    [host bringSubviewToFront:leftBorder];
+    [host bringSubviewToFront:rightBorder];
+}
+
+- (void)hideDebugBorder {
+    for (UIWindow *w in [UIApplication sharedApplication].windows) {
+        for (UIView *v in w.subviews) {
+            if (v.tag == 99991) [v removeFromSuperview];
+        }
+    }
 }
 
 @end
