7c056527bde0a03dd3236b2de1655695a86d2621
lingyangxue <lingyangxue@gmail.com>
2026-09-18 12:47:48 +0800

    Update HAAStatusBarIconManager.h

--- HideAllApps/HAAStatusBarIconManager.h
+++ HideAllApps/HAAStatusBarIconManager.h
@@ -3,4 +3,6 @@
 @interface HAAStatusBarIconManager : NSObject
 + (instancetype)sharedManager;
 - (void)refresh;
+- (void)noteAppBecameActive:(NSString *)bundleID;
+- (void)noteAppExited:(NSString *)bundleID;
 @end
