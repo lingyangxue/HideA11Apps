b34ae8fac96e565a720285a3406f29b26d9394f8
lingyangxue <lingyangxue@gmail.com>
2026-09-18 10:26:09 +0800

    Update HAAStatusBarIconManager.h

--- HideAllApps/HAAStatusBarIconManager.h
+++ HideAllApps/HAAStatusBarIconManager.h
@@ -1,14 +1,6 @@
 #import <UIKit/UIKit.h>
 
 @interface HAAStatusBarIconManager : NSObject
-@property (nonatomic, strong) UIView *container;
-@property (nonatomic, strong) NSMutableArray *iconViews;
-@property (nonatomic, strong) NSMutableArray *visibleBundleIDs;
-@property (nonatomic, strong) NSTimer *pollTimer;
-@property (nonatomic, weak) UIWindow *registeredStatusBarWin;
 + (instancetype)sharedManager;
-- (void)registerStatusBarWindow:(UIWindow *)win;
-- (void)setupIfNeeded;
 - (void)refresh;
-- (void)teardown;
 @end
