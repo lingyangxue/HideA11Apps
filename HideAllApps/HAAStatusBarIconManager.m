0a04252fb9657976cc28dc7dea0e5378f1cbd1a8
lingyangxue <lingyangxue@gmail.com>
2026-09-18 11:46:59 +0800

    Update HAAStatusBarIconManager.m

--- HideAllApps/HAAStatusBarIconManager.m
+++ HideAllApps/HAAStatusBarIconManager.m
@@ -1,6 +1,5 @@
 #import "HAAStatusBarIconManager.h"
 #import <notify.h>
-#import <objc/message.h>
 
 #define kSuiteName @"com.yourname.hideallapps"
 #define kDarwinNotification "com.yourname.hideallapps/prefsChanged"
@@ -27,15 +26,12 @@
     if (self) {
         _iconViews = [NSMutableArray array];
         _visibleBundleIDs = [NSMutableArray array];
-
         __weak typeof(self) weakSelf = self;
         static int token = 0;
         notify_register_dispatch(kDarwinNotification, &token, dispatch_get_main_queue(), ^(int t) {
             [weakSelf updatePollingState];
             [weakSelf refresh];
         });
-
-        // 监听 App 激活 / 退出通知（iOS 17 上这两个通知依然有效）
         [[NSNotificationCenter defaultCenter] addObserver:self
                                                  selector:@selector(appBecameActive:)
                                                      name:@"SBApplicationDidBecomeActiveNotification"
@@ -44,15 +40,6 @@
                                                  selector:@selector(appDidExit:)
                                                      name:@"SBApplicationDidExitNotification"
                                                    object:nil];
-        [[NSNotificationCenter defaultCenter] addObserver:self
-                                                 selector:@selector(appBecameActive:)
-                                                     name:@"UIApplicationDidBecomeActiveNotification"
-                                                   object:nil];
-        [[NSNotificationCenter defaultCenter] addObserver:self
-                                                 selector:@selector(appDidEnterBackground:)
-                                                     name:@"UIApplicationDidEnterBackgroundNotification"
-                                                   object:nil];
-
         dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3.0 * NSEC_PER_SEC)),
                        dispatch_get_main_queue(), ^{
             [weakSelf updatePollingState];
@@ -89,41 +76,33 @@
     return [v doubleValue];
 }
 
-#pragma mark - Overlay Window
-
 - (void)ensureOverlayWindow {
     if (self.overlayWindow) return;
-
     CGRect screen = [UIScreen mainScreen].bounds;
     self.overlayWindow = [[UIWindow alloc] initWithFrame:screen];
     self.overlayWindow.windowLevel = UIWindowLevelStatusBar + 1000;
     self.overlayWindow.backgroundColor = [UIColor clearColor];
     self.overlayWindow.userInteractionEnabled = NO;
     self.overlayWindow.hidden = NO;
-
     UIViewController *vc = [[UIViewController alloc] init];
     vc.view.backgroundColor = [UIColor clearColor];
     vc.view.frame = screen;
     self.overlayWindow.rootViewController = vc;
-
     self.container = [[UIView alloc] init];
     self.container.backgroundColor = [UIColor clearColor];
     self.container.userInteractionEnabled = NO;
     [vc.view addSubview:self.container];
 }
 
-#pragma mark - Polling
-
 - (void)updatePollingState {
     if ([self isEnabled]) {
         if (!self.pollTimer) {
-            self.pollTimer = [NSTimer scheduledTimerWithTimeInterval:0.6
+            self.pollTimer = [NSTimer scheduledTimerWithTimeInterval:0.8
                                                               target:self
                                                             selector:@selector(pollRunningApps)
                                                             userInfo:nil
                                                              repeats:YES];
         }
-        [self pollRunningApps];
     } else {
         if (self.pollTimer) {
             [self.pollTimer invalidate];
@@ -135,80 +114,50 @@
 
 - (NSArray *)runningBundleIDs {
     NSMutableArray *running = [NSMutableArray array];
-
     Class appCtrlClass = NSClassFromString(@"SBApplicationController");
     if (!appCtrlClass) return running;
-
     id shared = nil;
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Warc-performSelector-leaks"
     if ([appCtrlClass respondsToSelector:@selector(sharedInstance)]) {
         shared = [appCtrlClass performSelector:@selector(sharedInstance)];
     }
     if (!shared && [appCtrlClass respondsToSelector:@selector(sharedInstanceIfExists)]) {
         shared = [appCtrlClass performSelector:@selector(sharedInstanceIfExists)];
     }
-    if (!apps && [shared respondsToSelector:NSSelectorFromString(@"applications")]) {
-        apps = [shared performSelector:NSSelectorFromString(@"applications")];
-    }
-    }
-    if (!apps(@" && [shared respondsToSelector:NSSelectorFromString(@"applications")]) {
-        apps = [shared performSelector:NSSelectorFromString(@"applications")];
+    if (!shared) return running;
+    NSArray *apps = nil;
+    if ([shared respondsToSelector:@selector(allApplications)]) {
+        apps = [shared performSelector:@selector(allApplications)];
     }
     if (!apps) return running;
-
     for (id app in apps) {
         NSString *bid = nil;
         if ([app respondsToSelector:@selector(bundleIdentifier)]) {
             bid = [app performSelector:@selector(bundleIdentifier)];
         }
-#pragma clang diagnostic pop
         if (!bid || bid.length == 0) continue;
         if ([bid hasPrefix:@"com.apple."]) continue;
-
-        // iOS 17 用 backgroundState 判断（0=未运行, 1=启动中, 2=前台, 3=后台）
         BOOL isRunning = NO;
-
-        if ([app respondsToSelector:NSSelectorFromString(@"backgroundState")]) {
-            NSInteger (*fn)(id, SEL) = (NSInteger (*)(id, SEL))objc_msgSend;
-            NSInteger state = fn(app, NSSelectorFromString(@"backgroundState"));
-            if (state >= 1) isRunning = YES;
-        }
-        if (!isRunning && [app respondsToSelector:@selector(isRunning)]) {
-            BOOL (*fn)(id, SEL) = (BOOL (*)(id, SEL))objc_msgSend;
-            isRunning = fn(app, @selector(isRunning));
+        if ([app respondsToSelector:@selector(isRunning)]) {
+            isRunning = [app performSelector:@selector(isRunning)];
         }
         if (!isRunning && [app respondsToSelector:NSSelectorFromString(@"isRunningOrSuspended")]) {
-            BOOL (*fn)(id, SEL) = (BOOL (*)(id, SEL))objc_msgSend;
-            isRunning = fn(app, NSSelectorFromStringisRunningOrSuspended"));
+            isRunning = [app performSelector:NSSelectorFromString(@"isRunningOrSuspended")];
         }
-        // 兜底：只要进程还存在就认为在运行
-        if (!isRunning && [app respondsToSelector:NSSelectorFromString(@"processState")]) {
-            NSInteger (*fn)(id, SEL) = (NSInteger (*)(id, SEL))objc_msgSend;
-            NSInteger state = fn(app, NSSelectorFromString(@"processState"));
-            if (state >= 2) isRunning = YES;
-        }
-
         if (!isRunning) continue;
         [running addObject:bid];
     }
-
     return running;
 }
 
 - (void)pollRunningApps {
     if (![self isEnabled]) return;
-
     NSArray *running = [self runningBundleIDs];
     NSArray *cur = [self.visibleBundleIDs copy];
-
     if (![cur isEqualToArray:running]) {
         [self.visibleBundleIDs setArray:running];
         [self refresh];
     }
 }
-
-#pragma mark - Notifications
 
 - (void)appBecameActive:(NSNotification *)note {
     NSDictionary *info = note.userInfo;
@@ -216,10 +165,7 @@
     if (!bid) {
         id obj = note.object;
         if (obj && [obj respondsToSelector:@selector(bundleIdentifier)]) {
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Warc-performSelector-leaks"
             bid = [obj performSelector:@selector(bundleIdentifier)];
-#pragma clang diagnostic pop
         }
     }
     if (bid.length && ![bid hasPrefix:@"com.apple."]) {
@@ -234,22 +180,14 @@
     [self pollRunningApps];
 }
 
-- (void)appDidEnterBackground:(NSNotification *)note {
-    [self pollRunningApps];
-}
-
-#pragma mark - Icon Rendering
-
 - (UIImage *)iconForBundleID:(NSString *)bundleID {
     if (!bundleID) return nil;
     SEL sel = NSSelectorFromString(@"_applicationIconImageForBundleIdentifier:format:scale:");
     if (![[UIImage class] respondsToSelector:sel]) return nil;
-
     NSMethodSignature *sig = [[UIImage class] methodSignatureForSelector:sel];
     NSInvocation *inv = [NSInvocation invocationWithMethodSignature:sig];
     inv.target = [UIImage class];
     inv.selector = sel;
-
     NSString *bid = bundleID;
     [inv setArgument:&bid atIndex:2];
     NSInteger fmt = 0;
@@ -257,7 +195,6 @@
     CGFloat sc = [UIScreen mainScreen].scale;
     [inv setArgument:&sc atIndex:4];
     [inv invoke];
-
     __unsafe_unretained UIImage *img = nil;
     [inv getReturnValue:&img];
     return img;
@@ -268,24 +205,18 @@
         [self teardown];
         return;
     }
-
     [self ensureOverlayWindow];
     if (!self.container || !self.overlayWindow) return;
-
     [self.overlayWindow setHidden:NO];
-
     for (UIView *v in self.iconViews) [v removeFromSuperview];
     [self.iconViews removeAllObjects];
-
     CGFloat size = [self iconSize];
     CGFloat spacing = 4;
     CGFloat x = 0;
-
     NSArray *bidsToShow = self.visibleBundleIDs;
     if (bidsToShow.count == 0) {
         bidsToShow = @[@"com.apple.Preferences"];
     }
-
     for (NSString *bid in bidsToShow) {
         UIImage *icon = [self iconForBundleID:bid];
         if (!icon) continue;
@@ -298,14 +229,12 @@
         [self.iconViews addObject:iv];
         x += size + spacing;
     }
-
     CGFloat winW = self.overlayWindow.bounds.size.width;
     CGFloat totalW = MAX(x - spacing, 1);
     CGFloat ratio = [self positionRatio];
     CGFloat startX = (winW - totalW) * ratio;
     if (startX < 4) startX = 4;
     if (startX + totalW > winW - 4) startX = winW - totalW - 4;
-
     CGFloat y = [self verticalOffset];
     self.container.frame = CGRectMake(startX, y, totalW, size);
 }
