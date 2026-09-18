c2a563a2d459e33a40ae8cb4e8441f939d64d369
lingyangxue <lingyangxue@gmail.com>
2026-09-18 20:43:53 +0800

    Update HAAManager.h

--- HideAllApps/HAAManager.h
+++ HideAllApps/HAAManager.h
@@ -8,14 +8,27 @@ extern NSString * const kHAAPrefsChangedDarwinNotification;
 @property (nonatomic, assign) BOOL statusBarSingleTapEnabled;
 @property (nonatomic, assign) BOOL statusBarDoubleTapEnabled;
 @property (nonatomic, assign) BOOL shakeEnabled;
+
+// 左侧下滑
 @property (nonatomic, assign) BOOL leftDownEnabled;
+@property (nonatomic, assign) BOOL leftDownRecoverEnabled;
+@property (nonatomic, assign) BOOL leftDownHideEnabled;
+
+// 右侧下滑
+@property (nonatomic, assign) BOOL rightDownEnabled;
+@property (nonatomic, assign) BOOL rightDownRecoverEnabled;
+@property (nonatomic, assign) BOOL rightDownHideEnabled;
+
+// 触发区域
 @property (nonatomic, assign) CGFloat zoneTopRatio;
 @property (nonatomic, assign) CGFloat zoneBottomRatio;
 @property (nonatomic, assign) CGFloat zoneWidth;
 @property (nonatomic, assign) BOOL debugBorderEnabled;
+
 @property (nonatomic, assign) BOOL hideAll;
 @property (nonatomic, strong) NSSet *hiddenBundleIDs;
 @property (nonatomic, strong) NSTimer *refreshTimer;
+
 + (instancetype)sharedManager;
 - (void)reload;
 - (BOOL)shouldHideBundleID:(NSString *)bundleID;
