4292a7116ba1b97515c906f74851cb010705849e
lingyangxue <lingyangxue@gmail.com>
2026-09-18 21:13:47 +0800

    Update HAAManager.h

--- HideAllApps/HAAManager.h
+++ HideAllApps/HAAManager.h
@@ -2,21 +2,20 @@
 
 extern NSString * const kHAASuiteName;
 extern NSString * const kHAAPrefsChangedDarwinNotification;
+extern NSString * const kHAActivationPassword;
 
 @interface HAAManager : NSObject
 @property (nonatomic, assign) BOOL enabled;
+@property (nonatomic, assign) BOOL activated;   // ← 新增
 @property (nonatomic, assign) BOOL statusBarSingleTapEnabled;
 @property (nonatomic, assign) BOOL statusBarDoubleTapEnabled;
 @property (nonatomic, assign) BOOL shakeEnabled;
-
 @property (nonatomic, assign) BOOL leftDownEnabled;
 @property (nonatomic, assign) BOOL rightDownEnabled;
-
 @property (nonatomic, assign) CGFloat zoneTopRatio;
 @property (nonatomic, assign) CGFloat zoneBottomRatio;
 @property (nonatomic, assign) CGFloat zoneWidth;
 @property (nonatomic, assign) BOOL debugBorderEnabled;
-
 @property (nonatomic, assign) BOOL hideAll;
 @property (nonatomic, strong) NSSet *hiddenBundleIDs;
 @property (nonatomic, strong) NSTimer *refreshTimer;
