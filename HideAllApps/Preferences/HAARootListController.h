fdbd96b71eb7552ad93c8a11ee98035e95f5ba2a
lingyangxue <lingyangxue@gmail.com>
2026-09-18 11:30:49 +0800

    Update HAARootListController.h

--- HideAllApps/Preferences/HAARootListController.h
+++ HideAllApps/Preferences/HAARootListController.h
@@ -28,8 +28,11 @@ enum {
     PSGroupCell = 0,
     PSLinkCell = 1,
     PSLinkListCell = 2,
+    PSListItemCell = 3,
+    PSTitleValueCell = 4,
     PSSwitchCell = 6,
-    PSButtonCell = 9
+    PSButtonCell = 9,
+    PSSliderCell = 5
 };
 
 @interface HAARootListController : PSListController
