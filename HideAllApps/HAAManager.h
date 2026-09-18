5d9bdb593062c7f60cb865910604168f8a65c475
lingyangxue <lingyangxue@gmail.com>
2026-09-18 23:36:09 +0800

    Update HAAManager.h

--- HideAllApps/HAAManager.h
+++ HideAllApps/HAAManager.h
@@ -5,16 +5,6 @@ extern NSString * const kHAAPrefsChangedDarwinNotification;
 
 @interface HAAManager : NSObject
 @property (nonatomic, assign) BOOL enabled;
-@property (nonatomic, assign) BOOL statusBarSingleTapEnabled;
-@property (nonatomic, assign) BOOL statusBarDoubleTapEnabled;
-@property (nonatomic, assign) BOOL shakeEnabled;
-@property (nonatomic, assign) BOOL leftDownEnabled;
-@property (nonatomic, assign) BOOL rightDownEnabled;
-@property (nonatomic, assign) BOOL hideStatusBar;   // ← 新增：状态栏独立开关
-@property (nonatomic, assign) CGFloat zoneTopRatio;
-@property (nonatomic, assign) CGFloat zoneBottomRatio;
-@property (nonatomic, assign) CGFloat zoneWidth;
-@property (nonatomic, assign) BOOL debugBorderEnabled;
 @property (nonatomic, assign) BOOL hideAll;
 @property (nonatomic, strong) NSSet *hiddenBundleIDs;
 @property (nonatomic, strong) NSTimer *refreshTimer;
@@ -29,6 +19,4 @@ extern NSString * const kHAAPrefsChangedDarwinNotification;
 - (void)applyHiddenStateToIconView:(id)iconView;
 - (void)startRefreshTimer;
 - (void)stopRefreshTimer;
-- (void)showDebugBorder;
-- (void)hideDebugBorder;
 @end
