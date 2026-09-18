3c39e345e1ababa8e621c655d17d4591d4c92aad
lingyangxue <lingyangxue@gmail.com>
2026-09-19 00:25:54 +0800

    Refactor HAARootListController to use PSListController

--- HideAllApps/Preferences/HAARootListController.h
+++ HideAllApps/Preferences/HAARootListController.h
@@ -1,39 +1,5 @@
-#import <UIKit/UIKit.h>
-
-@interface PSViewController : UIViewController
-@end
-
-@interface PSListController : PSViewController {
-    NSMutableArray *_specifiers;
-}
-@property (nonatomic, strong) NSString *title;
-- (NSArray *)specifiers;
-- (void)reloadSpecifiers;
-@end
-
-@interface PSSpecifier : NSObject
-+ (PSSpecifier *)groupSpecifierWithName:(NSString *)name;
-+ (PSSpecifier *)preferenceSpecifierNamed:(NSString *)name
-                                   target:(id)target
-                                      set:(SEL)set
-                                      get:(SEL)get
-                                   detail:(Class)detail
-                                     cell:(NSInteger)cell
-                                     edit:(Class)edit;
-- (void)setProperty:(id)property forKey:(id)key;
-- (id)propertyForKey:(id)key;
-@end
-
-enum {
-    PSGroupCell = 0,
-    PSLinkCell = 1,
-    PSLinkListCell = 2,
-    PSListItemCell = 3,
-    PSTitleValueCell = 4,
-    PSSwitchCell = 6,
-    PSButtonCell = 9,
-    PSSliderCell = 5
-};
+#import <Preferences/PSListController.h>
+#import <Preferences/PSSpecifier.h>
 
 @interface HAARootListController : PSListController
 @end
